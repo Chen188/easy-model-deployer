d8273fdb49e85f4f3ff5a036091876c8a59d6e16
Bin Chen <fengzhishuiwo@163.com>
2025-09-24 21:24:56 +0800

    Update qwen.py

--- src/emd/models/vlms/qwen.py
+++ src/emd/models/vlms/qwen.py
@@ -147,7 +147,40 @@ Model.register(
     )
 )
 
-
+Model.register(
+    dict(
+        model_id = "Qwen2.5-VL-3B-Instruct",
+        supported_engines=[vllm_qwen25vl72b_engine073],
+        supported_instances=[
+            g5d2xlarge_instance,
+            g5d4xlarge_instance,
+            g5d8xlarge_instance,
+            g5d12xlarge_instance,
+            g5d16xlarge_instance,
+            g5d24xlarge_instance,
+            g5d48xlarge_instance,
+            g6e2xlarge_instance,
+            local_instance
+        ],
+        supported_services=[
+            sagemaker_service,
+            sagemaker_async_service,
+            ecs_service,
+            local_service
+        ],
+        supported_frameworks=[
+            fastapi_framework
+        ],
+        allow_china_region=True,
+        huggingface_model_id="Qwen/Qwen2.5-VL-3B-Instruct",
+        modelscope_model_id="Qwen/Qwen2.5-VL-3B-Instruct",
+        require_huggingface_token=False,
+        application_scenario="vision llms for image understanding",
+        description="The latest series of Qwen2.5 VL",
+        model_type=ModelType.VLM,
+        model_series=QWEN2VL_SERIES
+    )
+)
 
 Model.register(
     dict(
